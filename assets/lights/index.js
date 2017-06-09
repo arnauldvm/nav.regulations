9baeb0944f90ba0c491c725d167957c021734a15
Arnauld Van Muysewinkel <arnauldvm@gmail.com>
2017-06-10 01:34:09 +0200

    :sparkles: Draw lights

---
+++ assets/lights/index.js
@@ -0,0 +1,72 @@
+'use strict';
+
+const LIGHT_SIZE = 10;
+const MARGIN = 40;
+
+const PositionE = module.exports.PositionE = Object.freeze({
+  MAST: 'M',
+  BOW: 'B',
+  STERN: 'S',
+  RIGHT: 'R',
+  LEFT: 'L',
+  name: {
+    'M': {en: "mast", fr: "mat"},
+    'B': {en: "bow", fr: "proue"},
+    'S': {en: "stern", fr: "poupe"},
+    'R': {en: "right", fr: "droite"},
+    'L': {en: "left", fr: "gauche"}
+  },
+  xyz: { // relative value between 0 and 1
+    // x: axis of the boat from 0 (stern) to 1 (bow)
+    // y: longitudinal from 0 (left) to 1 (right)
+    // z: vertical from 0 (see level) to 1 (top of the mast)
+    'M': [0.3, 0.5, 1],
+    'B': [1, 0.5, 0.25],
+    'S': [0, 0.5, 0.25],
+    'R': [0.5, 1, 0.25],
+    'L': [0.5, 0, 0.25]
+  }
+});
+
+const ColorE = module.exports.ColorE = Object.freeze({
+  WHITE: 'W',
+  RED: 'R',
+  GREEN: 'G',
+  YELLOW: 'Y',
+  rgb: {
+    'W': "#FFF",
+    'R': "#F00",
+    'G': "#0F0",
+    'Y': "#FF0"
+  }
+});
+
+const ViewE = module.exports.ViewE = Object.freeze({
+  FRONT: 'f',
+  REAR: 'r',
+  RIGHT: 'R',
+  LEFT: 'L',
+  horiz: {
+    'f': {axis: 1, dir: -1},
+    'r': {axis: 1, dir: +1},
+    'R': {axis: 0, dir: +1},
+    'L': {axis: 0, dir: -1}
+  }
+});
+
+module.exports.light = function(ctx, position, color, view) {
+  const xyz = PositionE.xyz[position];
+  const rgb = ColorE.rgb[color];
+  const horiz = ViewE.horiz[view];
+  ctx.save();
+  ctx.transform(1, 0, 0, -1, 0, ctx.canvas.height);
+  const height = ctx.canvas.width - 2*MARGIN;
+  const width = ctx.canvas.height - 2*MARGIN;
+  ctx.fillStyle = rgb;
+  const x = ctx.canvas.width / 2 + (horiz.dir*(xyz[horiz.axis]-0.5))*width;
+  const y = MARGIN + xyz[2]*width;
+  ctx.beginPath();
+  ctx.arc(x, y, LIGHT_SIZE, 0, Math.PI*2);
+  ctx.fill();
+  ctx.restore();
+}
--- renderer.js
+++ renderer.js
@@ -1,6 +1,11 @@
+const lights = require('./assets/lights');
 const canvas = document.getElementById('base');
 if (canvas.getContext) {
   const ctx = canvas.getContext('2d');
+  const view = lights.ViewE.FRONT;
+  lights.light(ctx, lights.PositionE.MAST, lights.ColorE.WHITE, view);
+  lights.light(ctx, lights.PositionE.RIGHT, lights.ColorE.GREEN, view);
+  lights.light(ctx, lights.PositionE.LEFT, lights.ColorE.RED, view);
 } else {
   // canvas unsupported
 }
