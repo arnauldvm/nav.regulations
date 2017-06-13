21fce02f00de34652e1e91fb42c757243809bb33
Arnauld Van Muysewinkel <arnauldvm@gmail.com>
2017-06-13 23:59:47 +0200

    :sparkles: Add select list and data file with boats descriptions

--- renderer.js
+++ renderer.js
@@ -1,9 +1,21 @@
+const lightSelect = document.getElementById('lightSelect');
+const lightsDataList = require("./data/lights.json");
+lightsDataList.forEach(function(lightData) {
+  var option = document.createElement("option");
+  option.value = lightData.definition;
+  option.text = lightData.description;
+  lightSelect.appendChild(option);
+});
+
 const lights = require('./assets/lights');
 const canvas = document.getElementById('base');
 if (canvas.getContext) {
   const ctx = canvas.getContext('2d');
-  const view = lights.ViewE.FRONT;
-  lights.lightsMultipleCompact(ctx, "MW+RG+LR", view);
+  lightSelect.onchange = function() {
+    var view = lights.ViewE.FRONT;
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    lights.lightsMultipleCompact(ctx, lightSelect.value, view);
+  };
 } else {
   // canvas unsupported
 }
